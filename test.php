0b917162f84406166c6ef3497b9311f054b52034
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-01-21 16:58:52 -0700

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 {"books-read":[
     {"Title":"Harry Potter","Author":"J K Rowling"},
     {"Title":"Game of thrones","Author":"George R. R. Martin"},
-    {"Title":"The Lord of the Rings":"J R R Tolkein"}
+    {"Title":"The Lord of the Rings","Author":"J R R Tolkein"}
     ]
 };
 ?>
