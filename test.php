dfc759ad8fd1b4c3e4ab85915fc12b7bd98ef7c6
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-01-27 10:46:28 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,24 +1,5 @@
 <?php
-$servername = "cs-3260.database.windows.net";
-$username = "cs-3260@cs3620sql";
-$password = "Loveyoudad95";
-$dbname = "CS3260";
+header("Access-Control-Allow-Origin: *");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO Shows (ShowID, ShowName)
-VALUES (1, 'Freinds')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+echo '{"books-read":[{"Title":"The Hobbit","Author":"J. R. R. Tolkien"},{"Title":"Harry Potter", "Author":"J.K.Rowling"}]}'
 ?>
