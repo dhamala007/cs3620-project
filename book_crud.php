be4afa826630e6a8d07cc1179c69b66c8ef15f73
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-02-02 12:20:40 -0700

    Books

--- book_crud.php
+++ book_crud.php
@@ -1,1 +1,27 @@
 
+<?php
+
+session_start();
+$servername = "aazuresql.mysql.database.azure.com";
+$username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
+$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
+$dbname = "cs-3260";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Books (BookId, BookName, BookAuthor)
+VALUES (1, 'Harry Potter and the Philosopher's Stone', 'J.K.Rowling')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
