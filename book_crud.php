9475385568c5db380d0d01ad75b8899dd17839fe
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-02-03 16:39:06 -0700

    final

--- book_crud.php
+++ book_crud.php
@@ -18,9 +18,32 @@ $sql = "INSERT INTO Books (BookId, BookName, BookAuthor)
 VALUES (1, 'Harry Potter and the Philosophers Stone', 'J.K.Rowling')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "New record created successfully\r\n";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+echo"---------------------------------\r\n";
+
+$sql1 = "SELECT BookId, BookName, BookAuthor FROM `cs-3260`.Books";
+$result = $conn->query($sql1);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "This is the select statement!!!!!!!!!!\r\n";
+    echo "id: " . $row["BookId"]. " - Name: " . $row["BookName"]. " " . $row["BookAuthor"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+// sql to delete a record
+$sql2 = "DELETE FROM MyGuests WHERE id=1";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
 }
 
 $conn->close();
