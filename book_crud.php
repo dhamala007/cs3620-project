601c58b772bd37cb900957f188059ff93b550858
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-02-04 18:07:44 -0700

    Update book_crud.php

--- book_crud.php
+++ book_crud.php
@@ -5,7 +5,7 @@ session_start();
 $servername = "aazuresql.mysql.database.azure.com";
 $username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 $password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-$dbname = "cs-3260";
+$dbname = "cs-3620";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
