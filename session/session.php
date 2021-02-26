162594854e5f7762126dda7bd81d7c723e491b2a
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-02-26 11:36:46 -0700

    Add files via upload

---
+++ session/session.php
@@ -0,0 +1,24 @@
+<?php
+require_once('./user/user.php');
+
+class session {
+  // Methods
+  function login($username, $password) {
+     
+    $user = new User();
+    $loggedInUser = $user->checkLogin($username, $password);
+    
+    if($loggedInUser != 0){
+      $_SESSION["loggedIn"] = true;
+      $_SESSION["user_id"] = $loggedInUser;
+      return true;
+    }
+    else{
+      unset($_SESSION["loggedIn"]);
+      unset($_SESSION["user_id"]);
+      return false;
+    }
+  }
+
+}
+?>
