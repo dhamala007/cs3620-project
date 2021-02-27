0402be3485787c65af79ed995a8da3192e3dedac
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-02-27 14:10:37 -0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php
-
+    
     session_start();
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    
     require_once('./session/session.php');
+   
 
     $session = new session();
-    $login_result = $session->login($_POST["Username"], $_POST["Password"]);
-    
+    $login_result = $session->login($_POST["username"], $_POST["password"]);
     if(!$login_result){
         header("Location: login.html?error=true");
         exit();
