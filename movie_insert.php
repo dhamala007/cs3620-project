e0247f0764dd084fcd80698630d559d44d68f2c5
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-03-11 15:37:10 -0700

    Add files via upload

---
+++ movie_insert.php
@@ -0,0 +1,17 @@
+<?php
+header("Location: dashboard.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require_once('./movie/movie.php');
+
+$user = new movie();
+$user->setmoviename($_POST["movie_name"]);
+$user->setmoviedescription($_POST["movie_description"]);
+$user->setmovierating($_POST["movie_rating"]);
+$user->createMovie();
+
+?>
