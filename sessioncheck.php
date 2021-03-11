df4718209ae5885701575db6833a6839c25a3d49
dhamala007 <71794695+dhamala007@users.noreply.github.com>
2021-03-11 15:36:39 -0700

    Add files via upload

--- sessioncheck.php
+++ sessioncheck.php
@@ -2,8 +2,6 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    
-    session_start();
 
     if(!isset($_SESSION["loggedIn"])){
         header("Location: login.html");
